a5df3f7cfee2f9dcd66c6c1e5d51c1fcf4797358
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-08 11:11:35 -0500

    feat: Added App

---
+++ src/test/java/com/bootcamp/everis/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.everis.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
